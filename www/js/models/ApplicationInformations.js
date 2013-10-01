e4829a627c5fa1636e0777b4d6b49214665ead58
jerome <jerome.guiard@gmail.com>
2013-10-01 19:45:19 +0800

    add comment

--- www/js/models/ApplicationInformations.js
+++ www/js/models/ApplicationInformations.js
@@ -1,13 +1,27 @@
+/** Represent all informations
+ *
+ * */
 function applicationInfos(){
     new Rest("/api/v1/content_type/", "GET" ,{"limit" : "100"},{},null, storeContentType );
-    new Rest("/api/v1/category/", "GET" ,{"limit" : "45"},{},null, storeCategory ) ;
-
+    new Rest("/api/v1/category/", "GET" ,{"limit" : "45"},{},null, storeCategory );
 }
 
+/**
+ *CallBack function to store element retrieve from the server regarding the ContentType
+ *
+ * @param response {string} - Represent the api key
+ *
+ * */
 function storeContentType(response){
     window.localStorage.setItem("content_type",JSON.stringify(response));
 }
 
+/**
+ *CallBack function to store element retrieve from the server regarding the Category
+ *
+ * @param response {string} - Represent the api key
+ *
+ * */
 function storeCategory(response){
     window.localStorage.setItem("category",JSON.stringify(response));
 }
